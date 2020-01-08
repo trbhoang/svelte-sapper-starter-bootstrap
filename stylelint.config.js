4a5671e2def9373cd70c5d3b54fce5ea9f3febc9
dkodeit <dk0d3it@gmail.com>
2020-01-08 10:38:55 -0800

    add linting and formatting

---
+++ stylelint.config.js
@@ -0,0 +1,18 @@
+'use strict'
+
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+    'stylelint-config-prettier'
+  ],
+  rules: {
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global']
+      }
+    ],
+    'at-rule-no-unknown': null
+  }
+}
